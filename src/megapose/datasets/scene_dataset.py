dc7c9a69a9abf1fc69a69932d12f572db8a7ab18
Georgios Floros <georgiosfloros@gmail.com>
2024-11-26 22:34:50 +0200

    Script to run on LMO

--- src/megapose/datasets/scene_dataset.py
+++ src/megapose/datasets/scene_dataset.py
@@ -122,6 +122,25 @@ class ObjectData:
                 setattr(data, k, np.array(d[k]))
         return data
 
+    @staticmethod
+    def from_bop(bbox: DataJsonType, gt: DataJsonType, id_label_map: dict[int, str]) -> List[ObjectData]:
+        object_data = []
+        for bbox_data, gt_data in zip(bbox, gt):
+            obj_id = gt_data["obj_id"]
+            obj_data = ObjectData(label=id_label_map[obj_id])
+            bbox_obj = bbox_data["bbox_obj"]
+            bbox_visib = bbox_data["bbox_visib"]
+            bbox_obj[2] += bbox_obj[0]
+            bbox_obj[3] += bbox_obj[1]
+            bbox_visib[2] += bbox_visib[0]
+            bbox_visib[3] += bbox_visib[1]
+            setattr(obj_data, "bbox_amodal", np.array(bbox_obj))
+            setattr(obj_data, "bbox_modal", np.array(bbox_visib))
+            setattr(obj_data, "visib_fract", bbox_data["visib_fract"])
+            setattr(obj_data, "unique_id", obj_id)
+            object_data.append(obj_data)
+        return object_data
+
 
 @dataclass
 class CameraData:
